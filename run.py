a82c008a6f42f9a9f670ce1601f3551292da548a
Tess2019 <terese_burman@hotmail.com>
2020-10-11 10:22:34 +0000

    refreshed links in base

--- run.py
+++ run.py
@@ -3,24 +3,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route("/about")
+@app.route('/about')
 def about():
-    return render_template("about.html")    
+    return render_template("about.html")
 
-@app.route ("/contact")
+@app.route('/contact')
 def contact():
-    return render_template("contact.html")    
+    return render_template("contact.html")
 
-@app.route("/careers")
-def careers ():
-     return render_template("careers.html")
-
-
-if __name__ == "__main__": 
-    app.run(host=os.environ.get("IP"),
-            port=int(os.environ.get("PORT")),
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
