14730cbc931eea053659c61198901284c58d409b
Tess2019 <terese_burman@hotmail.com>
2020-10-20 09:30:36 +0000

    done with chapter reading from json file

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,18 +9,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route('/about')
 def about():
-    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company= data)
+
 
 @app.route('/contact')
 def contact():
     return render_template("contact.html", page_title="Contact")
 
+
 @app.route('/careers')
 def careers():
     return render_template("careers.html", page_title="Careers")
-
 
 
 if __name__ == '__main__':
